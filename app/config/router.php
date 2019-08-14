890bf4309bff67f2857d18f88b6363c6425ffed5
nhan98453 <nhan98453@gmail.com>
2019-08-14 17:58:11 +0700

    day2

--- app/config/router.php
+++ app/config/router.php
@@ -1,7 +1,27 @@
 <?php
+use Phalcon\Mvc\Router\Group;
+use Phalcon\Mvc\Router;
 
 $router = $di->getRouter();
 
-// Define your routes here
+$userGroup = new Group(['controller' => 'xxx']);
+const ROUTE_USER_GROUPS = '/user-groups';
+
+$userGroup->setPrefix(ROUTE_USER_GROUPS);
+$userGroup->addGet('', ['action' => 'get']);
+$userGroup->addPut('/{id}', ['action' => 'update']);
+$userGroup->addPost('', ['action' => 'post']);
+$userGroup->addDelete('/{id}', ['action' => 'delete']);
+
+$router->mount($userGroup);
+
+
+$router->notFound(
+    [
+        'controller' => 'index',
+        'action'     => 'route404',
+    ]
+);
 
 $router->handle();
+
